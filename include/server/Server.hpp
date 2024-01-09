62a4bf6b5ab89ca8bddb069f4f8813ccc4a85f1f
Marouane <procompte97@gmail.com>
2024-01-09 06:17:09 +0100

    [FIX]removed error of having an extra line in userRegistration()

--- include/server/Server.hpp
+++ include/server/Server.hpp
@@ -59,7 +59,8 @@ public :
 
             //	checks if msg revceived has a '\n'
     void    ReadIncomingMsg(std::string buff, std::map<int, std::string> &map,
-                                const std::vector<struct pollfd>  &fds, unsigned long &i);
+                                const std::vector<struct pollfd>  &fds, unsigned long &i
+                                , std :: vector<std :: string> &commands);
 
 private :
 
--- src/server/registrationCommands.cpp
+++ src/server/registrationCommands.cpp
@@ -7,12 +7,12 @@ void    parsePass(Client &, std::string str, const std::string &)
 {
     std::cout << "PASS : " << str << std::endl;
 
-    std::stringstream   ss(str);
-    std::string         token;
-    ss >> token;
-    if (token.compare("PASS") != 0) {
-        throw std::invalid_argument("Password Incorrect");
-    }
+//     std::stringstream   ss(str);
+//     std::string         token;
+//     ss >> token;
+    // if (token.compare("PASS") != 0) {
+    //     throw std::invalid_argument("Password Incorrect");
+    // }
 }
 
 void    parseNick(Client &, std::string str)
