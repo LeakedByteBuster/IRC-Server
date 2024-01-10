00fbbd7cf1d5070a7b2400f49da7f4e1f5a16b32
Marouane <procompte97@gmail.com>
2024-01-10 07:25:53 +0100

    [FIX] finished parsings PASS command

--- src/server/registrationCommands.cpp
+++ src/server/registrationCommands.cpp
@@ -1,18 +1,43 @@
 #include <iostream>
 #include <sstream>
+#include <vector>
 #include "registrationCommands.hpp"
+#include "Client.hpp"
+#include "Errors.hpp"
 
 
-void    parsePass(Client &, std::string str, const std::string &)
+void    parsePass(Client &clt, std::string str, const std::string &pass)
 {
     std::cout << "PASS : " << str << std::endl;
 
-//     std::stringstream   ss(str);
-//     std::string         token;
-//     ss >> token;
-    // if (token.compare("PASS") != 0) {
-    //     throw std::invalid_argument("Password Incorrect");
-    // }
+    std::stringstream           ss(str);
+    std::vector<std::string>    tokens;
+    std::string                 token;
+    unsigned long               i = 0;
+    while (ss >> token) {
+        tokens.push_back(token);
+    }
+
+    /*
+    
+    
+    THE SENDMSG() FUNCTION SHOULD BE STATIC
+    
+    
+    */
+    if (tokens[0].compare("PASS") != 0) {
+        clt.sendMsg(clt, LogError::passErrors(clt.nickname, LogError::PASS_NOT_SUPLLIED));
+        return ;
+    }
+    
+    for (; i < tokens.size(); i++)
+        ;
+    if (tokens[i-1].compare(pass) != 0) {
+        clt.sendMsg(clt, LogError::passErrors(clt.nickname, LogError::INCORRECT_PASS));
+        return ;
+    }
+
+    clt.sendMsg(clt, LogError::passErrors(clt.nickname, LogError::CORRECT_PASS));
 }
 
 void    parseNick(Client &, std::string str)
