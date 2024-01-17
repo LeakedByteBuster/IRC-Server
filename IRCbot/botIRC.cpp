186b0c3183abe1abeae1b741dc2fb8105113e7f2
Marouane <procompte97@gmail.com>
2024-01-17 06:36:20 +0100

    [REFRACTOR] separate tasks in functions and added new ones

--- IRCbot/botIRC.cpp
+++ IRCbot/botIRC.cpp
@@ -1,11 +1,62 @@
 #include <iostream>
 #include <sys/socket.h> // socket(), bind(), listen(), connect(), accept()
 #include <arpa/inet.h> // struct sockaddr_in
+#include <unistd.h>
 
-static int  parseInput(const char *s, std::string pass)
+#define ERR_INVALIDARGUMENTS "Error :invalid number of arguments :<serverip> <port> <pass>"
+
+enum    Command {
+    JOKES,
+    TIME,
+    WELCOME
+};
+
+static const char *    getBigMsg()
+{
+        const char    *bigMsg =
+
+  "\n$$$$$$\\ $$$$$$$\\   $$$$$$\\  \n"
+        "\\_$$  _|$$  __$$\\ $$  __$$\\  \n"
+        "  $$ |  $$ |  $$ |$$ /   \n"
+        "  $$ |  $$$$$$$  |$$ | \n"
+        "  $$ |  $$  __$$< $$ |  \n"
+        "  $$ |  $$ |  $$ |$$ | \n"
+        "$$$$$$\\ $$ |  $$ |\\$$$$$$  |     \n"
+        "\\______|\\__|  \\__| \\______/   \n";
+    return (bigMsg);
+}
+
+static std::string  geTime()
+{
+    time_t  t;
+    time(&t);
+    std::string currentTime = asctime(localtime(&t));
+    currentTime.pop_back(); // removes '\n' character from the string
+    return (currentTime);
+}
+
+static bool    sendRegistrationInfo(const int fd, char *pass)
+{
+    std::string regs;
+
+    regs = "PASS " + static_cast<std::string>(pass)
+            + static_cast<std::string>("\n");
+    regs.append("NICK bot\n");
+    regs.append("USER bot 0 * bot");
+
+    std::cout << "Connected to the server successfully" << std::endl;
+
+    if (send(fd, regs.data(), regs.size(), 0) == -1) {
+        std::cerr << "Error send(): " << strerror(errno) << std::endl;
+        return (0);
+    }
+    return (1);
+}
+
+static int  parseInput(const char *port, std::string pass)
 {
     char    *endptr = NULL;
-    long res = strtol(s, &endptr, 10);
+    long res = strtol(port, &endptr, 10);
     if ((endptr && *endptr != '\0') || (res > USHRT_MAX)
         || (res < 0)) {
         throw std::invalid_argument("Error : invalid port number");
@@ -17,48 +68,71 @@ static int  parseInput(const char *s, std::string pass)
     return (0);
 }
 
-int main(int ac, char **av) 
+static int connectToServer(const char *ip, const char *port,
+        const char *pass, struct sockaddr_in &addr, socklen_t len)
 {
-    struct sockaddr_in  addr;
-    socklen_t           len = sizeof(addr);
-
-    if (ac != 4) {
-        std::cerr << "Error :invalid number of arguments :<serverip> <port> <pass>"
-            << std::endl;
-        return (1);
-    }
     memset(&addr, 0, sizeof(addr));
     try {
-        parseInput(av[2], av[3]);
+        parseInput(port, pass);
     } catch (std::exception &e) {
         std::cout << e.what() << std::endl;
-        return (1);
+        return (0);
     }
 
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd == -1) {
         std::cerr << "Error socket():" << strerror(errno) << std::endl;
-        return (2);
+        return (0);
     }
 
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(atoi(av[2]));
-    if (inet_pton(addr.sin_family, av[1], &addr.sin_addr) < 0) {
+    addr.sin_port = htons(atoi(port));
+    if (inet_pton(addr.sin_family, ip, &addr.sin_addr) < 0) {
         std::cerr << "Error inet_pton(): " << strerror(errno) << std::endl;
-        return (2);
+        return (0);
     }
 
-    int status = 0;
-    if ((status = connect(fd, (const sockaddr *)&addr, len)) == -1) {
+    if (connect(fd, (const sockaddr *)&addr, len) == -1) {
         std::cerr << "Error connect(): " << strerror(errno) << std::endl;
+        return (0);
+    }
+    return (fd);
+}
+
+int main(int ac, char **av) 
+{
+    struct sockaddr_in  addr;
+    socklen_t           len = sizeof(addr);
+    int                 fd = 0;
+    char                buff[4096];
+    int                 bytes = 0;
+
+    if (ac != 4) {
+        std::cerr << ERR_INVALIDARGUMENTS << std::endl;
+        return (1);
+    }
+    if ((fd = connectToServer(av[1], av[2], av[3], addr, len)) == 0) {
+        return (2);
+    }
+    if (!sendRegistrationInfo(fd, av[3])) {
         return (2);
     }
 
-    std::cout << "Connected to the server successfully" << std::endl;
-    // while (1) {
-    //     sleep(60);
-
-    // }
+    memset(buff, 0, sizeof(buff));
+    while (1) {
+        std::cout << "Waiting..." << std::endl;
+        if ((bytes = recv(fd, static_cast<void *>(buff), sizeof(buff) - 1, 0)) == -1) {
+            std::cerr << "Error recv(): " << strerror(errno) << std::endl;
+            return (2);
+        } else if (bytes == 0) {
+            std::cout << "Server disconnected" << std::endl;
+            break ;
+        }
+        std::cout << buff << std::endl;
+        std::cout.flush();
+        // enumtype  parseBuff();
+        // void      executeBuff();
+    }
 
     return (0);
 }
