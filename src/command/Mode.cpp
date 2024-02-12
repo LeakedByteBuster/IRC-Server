b6f2286dbaacf669a7bb91bb856b3b864dd87a67
Marouane <procompte97@gmail.com>
2024-02-12 05:26:05 +0100

    [FIX] finishes mode t and l

--- src/command/Mode.cpp
+++ src/command/Mode.cpp
@@ -42,6 +42,19 @@ static std::string  appendModeArgs(std::vector<std::string> vec)
     return (args);
 }
 
+static inline void    putSign(std::string &res, bool sign, bool &pSign, bool &mSign)
+{
+    if (sign == 1 && !pSign) {
+        res.append("+");
+        pSign = 1;
+        mSign = 0;
+    } else if (sign == 0 && !mSign) {
+        res.append("-");
+        pSign = 0;
+        mSign = 1;
+    }
+}
+
 /* -------------------------------------------------------------------------- */
 /*                                  Parsing                                   */
 /* -------------------------------------------------------------------------- */
@@ -63,10 +76,9 @@ static std::vector<std::string>    parseModeString(std::string ms, std::vector<s
 {
     std::string                 err;
     std::vector<std::string>    buff = splitByValue(ms, '+');
-    size_t                      argSize = (args.size() > 0) ? args.size(): 0; // to change error
+    size_t                      argSize = (args.size() > 0) ? args.size() : 0;
     bool                        minus; // used to check if a minus char is seen
 
-    printLog(buff);
     for (size_t i = 0; i < buff.size() ; i++) {
         minus = 0;
         for (size_t j = 0; j < buff[i].size(); j++) {
@@ -91,11 +103,8 @@ static std::vector<std::string>    parseModeString(std::string ms, std::vector<s
                 }
                 throw error_pair(ERR_NEEDMOREPARAMS, 1);
             }
-
         }
-
     }
-
     return (buff);
 }
 
@@ -153,7 +162,7 @@ static t_Modes   separateModes(std::vector<std::string> mString, std::vector<std
     size_t  n = 0; // used for param arguments
 
     for (size_t i = 0; i < mString.size(); i++) {
-        
+
         size_t  pos = 0; // used for substr in case there's a '-' char
 
         /* Insert all characters in mString[i] as plus modes. No '-' char is found  */
@@ -161,19 +170,14 @@ static t_Modes   separateModes(std::vector<std::string> mString, std::vector<std
             insertPlusModes(param, modesVecs, mString[i], n);
             continue ;
         }
-
         /* Insert plus and minus modes each in its proper variable in t_modes var */
         std::vector<std::string>    modesStr = splitByValue(mString[i], '-'); // modeStr[0] are plus modes, rest is minus modes
         if (modesStr.size() == 0) { continue ; } // no modes were given
-        
-        // printLog(modesStr);
         if (mString[i][0] != '-') {
             insertPlusModes(param, modesVecs, modesStr[0], n); // insert plus modes
-            modesStr.erase(modesStr.begin()); // deletes  modesStr[0] which is a string of plus modes
+            modesStr.erase(modesStr.begin()); // deletes modesStr[0] which is a string of plus modes
         }
-        // printLog(modesStr);
         insertMinusModes( param, modesVecs, modesStr, n ); // insert minus modes in vector modesStr
-
     }
     return (modesVecs);
 }
@@ -184,10 +188,6 @@ static t_Modes   separateModes(std::vector<std::string> mString, std::vector<std
 
 // typedef std::vector< std::pair< std::pair< std::string, std::string >, bool > > modesList;
 
-// static bool mode_L(Channel &ch, const bool &sign)
-// {
-//     return ;
-// }
 
 // static bool mode_O(std::map<int, Client> &clients, Client &clt, std::string arg, const bool &sign)
 // {
@@ -195,10 +195,6 @@ static t_Modes   separateModes(std::vector<std::string> mString, std::vector<std
 //     return ;
 // }
 
-// static bool mode_T(Channel &ch, const bool &sign)
-// {
-//     return ;
-// }
 
 // static bool mode_K(Channel &ch, std::string key, const bool &sign)
 // {
@@ -206,15 +202,35 @@ static t_Modes   separateModes(std::vector<std::string> mString, std::vector<std
 //     return ;
 // }
 
-/*
-------------- 'i' MODE
-MODE #1997 -i [ignores it silently, if not already set]
+static bool mode_L(Channel &ch, std::string param, const bool &sign)
+{
+    if ((sign && (ch.isUsersLimit == 1)) || (!sign && (ch.isUsersLimit == 0)))
+        return (0);
 
-MODE #1997 +i
-    :Jack!~hoba@197.230.30.146 MODE #1997 +i 
-MODE #986 -i jk hkjhk jh
-    :apo98s!~apo@197.230.30.146 MODE #986 -i 
-*/
+    if (!param.empty()) {
+        char    *endptr = NULL;
+        long    res = std::strtol(param.data(), &endptr, 10);
+        if ((*endptr != '\0') || (errno == ERANGE) || res < 1 || res > USERS_CHANNEL_LIMIT) {
+            return (0);
+        }
+        ch.isUsersLimit = 1;
+        ch.usersLimit = res;
+        return (1);
+    }
+
+    (sign == 1) ? ch.isUsersLimit = 1 : ch.isUsersLimit = 0;
+
+    return (1);
+}
+
+static bool mode_T(Channel &ch, const bool &sign)
+{
+    if ((sign && (ch.isTopic == 1)) || (!sign && (ch.isTopic == 0)))
+        return (0);
+    (sign == 1) ? ch.isTopic = 1 : ch.isTopic = 0;
+    return (1);
+}
+
 static bool mode_I(Channel &ch, const bool &sign)
 {
     if ((sign && (ch.isInviteOnly == 1)) || (!sign && (ch.isInviteOnly == 0)))
@@ -249,27 +265,29 @@ static std::string    setModes(std::map<int, Client> &, Client &, const modesLis
     bool        sign;
     bool        pSign = 0;
     bool        mSign = 0;
-    // std::map<>
 
     for (size_t i = 0; i < list.size(); i++) {
         mode = list[i].first.first;
         param = list[i].first.second;
         sign = list[i].second;
-        if (sign == 1 && !pSign) {
-            res.append("+");
-            pSign = 1;
-            mSign = 0;
-        } else if (sign == 0 && !mSign) {
-            res.append("-");
-            pSign = 0;
-            mSign = 1;
-        }
+
 
         if ((mode.compare("i") == 0) && mode_I(ch, sign)) {
+            putSign(res, sign, pSign, mSign);
             res.append(mode);
+            continue ;
+        }
+        if ((mode.compare("t") == 0) && mode_T(ch, sign)) {
+            putSign(res, sign, pSign, mSign);
+            res.append(mode);
+            continue ;
+        }
+        if ((mode.compare("l") == 0) && mode_L(ch, param, sign)) {
+            putSign(res, sign, pSign, mSign);
+            res.append(mode);
+            continue ;
         }
     }
-    printLog(res, "res --> ");
     return (res);
 }
 
@@ -302,9 +320,13 @@ void    Operator::mode(std::map<int, Client> &clients, Client &clt, const std::v
         // }
         // // **************************************************************************************** //
 
-        setModes(clients, clt, modes.toExecute, ch);
+        std::string res = setModes(clients, clt, modes.toExecute, ch);
 
+        printLog(res, "mode string --> ");
         printLog(ch.isInviteOnly, "is invite : ");
+        printLog(ch.isTopic, "is topic : ");
+        printLog(ch.isUsersLimit, "is userLimit : ");
+        printLog(ch.usersLimit, "userLimit = ");
 
     } catch (error_pair error) {
         if (error.second == 1)
@@ -337,7 +359,13 @@ MODE #986 +l 2147483647
 MODE #986 +l
     :mercury.libera.chat 461 apo98s MODE :Not enough parameters
 
+------------- 'i' MODE
+MODE #1997 -i [ignores it silently, if not already set]
 
+MODE #1997 +i
+    :Jack!~hoba@197.230.30.146 MODE #1997 +i 
+MODE #986 -i jk hkjhk jh
+    :apo98s!~apo@197.230.30.146 MODE #986 -i 
 
 ------------- 'o' MODE
 MODE #986  +o 02 [ignores it silently]
